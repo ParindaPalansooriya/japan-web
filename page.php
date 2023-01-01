ab4e2bcde2e0fdd7e548a6955a12f02582ede39a
ParindaPalansooriya <parindapalansooriya@gmail.com>
2023-01-01 09:07:10 +0530

    php

--- page.php
+++ page.php
@@ -8,13 +8,61 @@
 <body>
 <?php
     require_once "./php/car_dao.php";
-    // FunctionName(null);
-    // insertCarFull(1,1,1,1,1,1,1,1,"ww","ww","ww","ww","ww","ww","ww","ww","ww","ww","ww","ww","ww","ww",1,1,1);
-    $arra = getAllCars();
+    $arra = searchString("w",null,null);
     for ($i=0; $i < sizeof($arra); $i++) { 
         echo $arra[$i]->maker_id;
         echo '</br>';
     }
+    echo '</br>';
+
+    $arra = searchString("w",null,null);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
+
+    $arra = searchString("e",null,null);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
+
+    $arra = searchString("g",null,null);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
+
+    $arra = searchString("n",null,null);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
+
+    $arra = searchString("w",5,null);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
+
+    $arra = searchString("w",null,2);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
+
+    $arra = searchString("e",null,2);
+    for ($i=0; $i < sizeof($arra); $i++) { 
+        echo $arra[$i]->maker_id;
+        echo '</br>';
+    }
+    echo '</br>';
 ?>
 </body>
 </html>
--- php/car_dao.php
+++ php/car_dao.php
@@ -104,4 +104,68 @@ function getAllCars(){
     return $retuen_val;
 }
 
+function searchString($val,$modulId,$makerId){
+    $retuen_val = [];
+    require_once "config.php";
+    require_once "car_module.php";
+    if(empty($a)){
+        if(isset($modulId) && !isset($makerId)){
+            $sql2 = "SELECT * FROM cars where model_id = $modulId";
+        }else if(!isset($modulId) && isset($makerId)){
+            $sql2 = "SELECT * FROM cars where maker_id = $makerId";
+        }else if(isset($modulId) && isset($makerId)){
+            $sql2 = "SELECT * FROM cars where model_id = $modulId and maker_id = $makerId";
+        }else{
+            $sql2 = "SELECT * FROM cars";
+        }
+    }else{
+        if(isset($modulId) && !isset($makerId)){
+            $sql2 = "SELECT * FROM cars where model_id = $modulId and name like '%$val%' or grade like '%$val%' or note like '%$val%'";
+        }else if(!isset($modulId) && isset($makerId)){
+            $sql2 = "SELECT * FROM cars where maker_id = $makerId and name like '%$val%' or grade like '%$val%' or note like '%$val%'";
+        }else if(isset($modulId) && isset($makerId)){
+            $sql2 = "SELECT * FROM cars where model_id = $modulId and maker_id = $makerId and name like '%$val%' or grade like '%$val%' or note like '%$val%'";
+        }else{
+            $sql2 = "SELECT * FROM cars";
+        }
+    }
+
+    if($result = mysqli_query($link, $sql2)){
+        while($row = mysqli_fetch_array($result)){
+            $car = new Cars(
+                $row['maker_id'],
+                $row['model_id'],
+                $row['interior_color_id'],
+                $row['exterior_color_id'],
+                $row['current_action_id'],
+                $row['body_style_id'],
+                $row['passengers'],
+                $row['doors'],
+                $row['name'],
+                $row['grade'],
+                $row['power'],
+                $row['model_year'],
+                $row['evaluation'],
+                $row['running'],
+                $row['cooling'],
+                $row['note'],
+                $row['fuel'],
+                $row['chassis'],
+                $row['dimensions_L'],
+                $row['dimensions_W'],
+                $row['dimensions_H'],
+                $row['transmission_shift'],
+                $row['id'],
+                $row['is_public'],
+                $row['is_used'],
+                $row['is_two_weel'],
+                $row['is_steering_right']
+            );
+            array_push($retuen_val,$car);
+        }
+        mysqli_free_result($result);
+    }
+    return $retuen_val;
+}
+
 ?>
